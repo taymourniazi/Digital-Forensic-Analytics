7ddd44f7f5274fb0277bc31bbc81e0d35d75eeda
Taymour Khan Niazi <30389323+taymourniazi@users.noreply.github.com>
2018-07-21 14:05:04 +0500

    Allowing annonymous login

---
+++ I) anonLogin.py
@@ -0,0 +1,16 @@
+import ftplib
+
+def anonLogin(hostname):
+	try:
+		ftp = ftplib.FTP(hostname)
+		ftp.login('anonymous', 'me@your.com')
+		print '\n[*] ' + str(hostname) +\
+		' FTP Anonymous Logon Succeeded.'
+		ftp.quit()
+		return True
+	except Exception, e:
+		print '\n[-] ' + str(hostname) +\
+		' FTP Anonymous Logon Failed.'
+		return False
+host = '192.168.95.179'
+anonLogin(host)
