07017277104aa5885836a7dfbee74edec090fc0b
Taymour Khan Niazi <30389323+taymourniazi@users.noreply.github.com>
2018-07-24 10:52:57 +0500

    Add files via upload

---
+++ O) pdfRead.py
@@ -0,0 +1,26 @@
+import PyPDF2
+import optparse
+from PyPDF2 import PdfFileReader
+
+def printMeta(fileName):
+	pdfFile = PdfFileReader(file(fileName, 'rb'))
+	docInfo = pdfFile.getDocumentInfo()
+	print '[*] PDF MetaData For: ' + str(fileName)
+	for metaItem in docInfo:
+		print '[+] ' + metaItem + ':' + docInfo[metaItem]
+
+def main():
+	parser = optparse.OptionParser('usage %prog "+\
+		"-F <PDF file name>')
+	parser.add_option('-F', dest='fileName', type='string',\
+		help='specify PDF file name')
+	(options, args) = parser.parse_args()
+	fileName = options.fileName
+	if fileName == None:
+		print parser.usage
+		exit(0)
+	else:
+		printMeta(fileName)
+
+if __name__ == '__main__':
+	main()
